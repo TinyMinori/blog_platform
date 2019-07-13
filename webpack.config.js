7b7b5b9819bb170dab64b51fed9bdea88a14154f
Yoann <yoann.sanchez@epitech.eu>
2019-07-14 00:11:39 +0200

    Feat Favicon

--- webpack.config.js
+++ webpack.config.js
@@ -43,8 +43,8 @@ module.exports = {
         ]
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"]
+        test: /\.(png|svg|jpg|gif|ico)$/,
+        loader: "file-loader"
       }
     ]
   },

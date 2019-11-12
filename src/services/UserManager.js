1de9802f24e8c6b518dfc7bc81a54f21c0a4d08f
Yoann <yoann.sanchez@epitech.eu>
2019-11-12 13:00:32 -0500

    First User Management

--- src/services/UserManager.js
+++ src/services/UserManager.js
@@ -1,5 +1,33 @@
-exports.connect = () => {}
+import Cookies from "cookies-js"
 
-exports.isConnected = () => {}
+exports.connect = (username, password) => {
+  return new Promise((resolve, reject) => {
+    this.$http
+      .post(process.env.API_URL + "/login", {
+        username: username,
+        password: password
+      })
+      .then(ctx => {
+        if (Cookies.enabled) {
+          Cookies.set("token", ctx.body.token)
+          resolve()
+        } else
+          reject(
+            "Your browser doesn't allow cookies. Enable them to connect properly."
+          )
+      })
+      .catch(error => {
+        reject(error.body.message)
+      })
+  })
+}
 
-exports.isPublisher = () => {}
+exports.isConnected = () => {
+  if (Cookies.enabled && Cookies.get("token") !== undefined) return true
+  return false
+}
+
+exports.isPublisher = () => {
+  if (Cookies.enabled && Cookies.get("role") === "publisher") return true
+  return false
+}
